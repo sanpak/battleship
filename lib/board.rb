19bbea945e604a8ecc0050d50529060965a90ea1
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-06-05 22:55:16 -0400

    solving board rb

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,26 @@
 class Board
+  attr_accessor :grid
+  def self.default_grid
+    @grid = Array.new(10) { Array.new(10) }
+  end
+
+  def initialize(grid=nil)
+    @grid = grid
+    @grid = Board.default_grid unless grid
+  end
+
+  def count
+    puts " ________count__________"
+    ship_count = 0
+    @grid.each do |row|
+      row.each do |cell|
+        ship_count += 1 if cell == :s
+      end
+    end
+    ship_count
+  end
+
+  def empty?
+  end
+
 end
